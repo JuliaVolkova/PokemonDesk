c80952205039368d6784afd3f09cffb49fc2ba4e
Julia Volkova <julia.volkova789@gmail.com>
2020-11-03 23:18:05 +0300

    feat: Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-    return (
+    return <>
         <Header/>
-    )
+        <main/>
+        <Footer/>
+    </>
 }
 
 export default App;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import cn from 'classnames';
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+    return <footer className={cn(styles.footer)}>
+        <ul className={cn(styles.linksList)}>
+            <li className={cn(styles.listItem)}>Make with ❤</li>
+            <li className={cn(styles.listItem)}>Ours Team</li>
+        </ul>
+    </footer>
+}
+
+export default Footer;
