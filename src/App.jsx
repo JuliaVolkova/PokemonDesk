7168617c4c235006a3dbfc67626dacdfb5d8d278
Julia Volkova <julia.volkova789@gmail.com>
2020-11-03 23:47:01 +0300

    feat: Add routing with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const App = () => {
     return <>
-        <Header/>
-        <main/>
-        <Footer/>
+        <Router>
+            <Header/>
+            <Switch>
+                <Route path="/pokédex">
+                    <main>Pokédex</main>
+                </Route>
+                <Route path="/legendaries">
+                    <main>Legendaries</main>
+                </Route>
+                <Route path="/documentation">
+                    <main>Documentation</main>
+                </Route>
+                <Route path="/">
+                    <main>4</main>
+                </Route>
+            </Switch>
+            <Footer/>
+        </Router>
     </>
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import cn from 'classnames';
 import styles from './Header.module.scss';
@@ -10,10 +11,17 @@ const Header = () => {
         <nav className={cn(styles.navigation)}>
             <img src={logo} width={180} height={52} alt={'Pokemon desk logo'}/>
             <ul className={cn(styles.navigationList)}>
-                <li className={cn(styles.navItem)}>Home</li>
-                <li className={cn(styles.navItem)}>Pokédex</li>
-                <li className={cn(styles.navItem)}>Legendaries</li>
-                <li className={cn(styles.navItem)}>Documentation</li>
+                <li className={cn(styles.navItem)}>
+                    <NavLink to='/' className={cn(styles.navLink)}>Home</NavLink></li>
+                <li className={cn(styles.navItem)}>
+                    <NavLink to='/pokédex' className={cn(styles.navLink)}>Pokédex
+                    </NavLink></li>
+                <li className={cn(styles.navItem)}>
+                    <NavLink to='/legendaries' className={cn(styles.navLink)}>Legendaries
+                    </NavLink></li>
+                <li className={cn(styles.navItem)}>
+                    <NavLink to='/documentation' className={cn(styles.navLink)}>Documentation
+                    </NavLink></li>
             </ul>
         </nav>
     </header>
