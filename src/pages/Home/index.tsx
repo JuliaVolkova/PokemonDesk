3beaed2e4da49b759058ae64baffff06912f4734
Julia Volkova <julia.volkova789@gmail.com>
2020-11-08 22:53:05 +0300

    feat: Add button component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import styles from './Home.module.scss';
 import cn from 'classnames';
+import styles from './Home.module.scss';
+
+// @ts-ignore
+import Button from 'components/Button';
 
 const HomePage = ({ content }: { content: string }) => {
   return (
-    <div className={cn(styles.root)}>{content}</div>
+    <>
+      <div className={cn(styles.root)}>{content}
+        <Button children='See pokemons' onClick={()=> console.log('Click!!')}/>
+      </div>
+    </>
   );
 };
 
