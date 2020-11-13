adb4b0c580613d24e72e4be36c6b6ddfcc22623f
Julia Volkova <julia.volkova789@gmail.com>
2020-11-13 23:47:09 +0300

    feat: Add API integration with fetch

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import cn from 'classnames';
 // @ts-ignore
 import PokemonCard from 'components/PokemonCard';
 import styles from './Pokedex.module.scss';
 
-import { pokemons } from '../../pokemons';
+import Heading from '../../components/Heading';
+
+const API_URL = 'http://zar.hosthot.ru/api/v1/pokemons';
 
 const PokedexPage = () => {
-  return <main className={cn(styles.root)}>{
-    pokemons.map((pokemon, index) => <PokemonCard data={pokemon} key={pokemon.name} />)
-  }</main>;
+  const [totalPokemons, setTotalPokemons] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setTotalPokemons(data.total);
+        setPokemons(data.pokemons);
+      })
+      .catch(() => {
+        setIsError(true);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Something went wrong! :(</div>;
+  }
+
+  return <main className={cn(styles.root)}>
+    <Heading size='l' title={`${totalPokemons} Pokemons for you to choose your favorite`} />
+    <section className={cn(styles.content)}>
+      {pokemons.map((pokemon: {name: string}) => <PokemonCard data={pokemon} key={pokemon.name} />)}
+    </section>
+  </main>;
 };
 
 export default PokedexPage;
