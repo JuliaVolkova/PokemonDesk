e7afdf35c1686c20a20ca216ddfb2f9e6291657f
Julia Volkova <julia.volkova789@gmail.com>
2020-11-15 01:13:04 +0300

    chore: Change webpack config, .gitignore and add custom.d.ts

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
-  watch: true,
+  watch: NODE_ENV === 'development',
   watchOptions: {
     ignored: /node_modules/,
     poll: 1000
